a42a565f83771249a1d557855d8b760c00963da3
Katie Weber <k.weber0893@gmail.com>
2018-05-21 16:02:03 -0400

    Completed counting sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
+  end
 
+  def statement?
+    self.sentence? || self.question? || self.exclamation?
   end
 
   def count_sentences
-
+    counter = 0
+    self.split(" ").each do |word|
+      counter += 1 if word.statement?
+    end
+    counter
   end
 end
